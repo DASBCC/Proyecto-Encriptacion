5255f8e794585797c35ff062674d6ef7969640ab
Daniel Araya Sambucci <danielarayasambucci@gmail.com>
2020-06-12 20:27:44 -0600

    Función XOR y conversion a Decimal agregadas

--- Llave.py
+++ Llave.py
@@ -4,4 +4,22 @@ def llave(numero):
 def llave_aux(numero):
     Lista=[int(x) for x in str(numero)]
     return Lista
-print(llave(2))
+#print(llave(55))
+
+
+def XOR(n):
+    if len(n)<8:
+        return XOR([0]+n)
+    if len(n)==8:
+        return [(((n[7]^n[5])^n[3])^n[2])] + n[:-1]
+    
+#print(XOR(binario))
+
+def base2dec(nums, base):
+    if len(nums) == 1:
+        return nums[0]
+    else:
+        return nums[-1] + base * base2dec(nums[:-1], base)
+
+print (base2dec([1,1,1],2))
+
--- bin.py
+++ bin.py
@@ -5,4 +5,18 @@ def binario_aux(numero,contador,respuesta):
         return respuesta
     else:
         return binario_aux(numero//2,contador+1,respuesta+(numero%2)*10**contador)
-print (binario(233))
+#print (binario(233))
+
+def CD(a):
+    if a<10:
+        return 1
+    else:
+        return 1 + CD(a//10)
+
+def Decimal(n):
+    if n<10:
+        return n 
+    else:
+        return (n//10**(CD(n)-1))* 2 ** (CD(n)-1) + Decimal(n%10**(CD(n)-1))
+
+#print (Decimal(111))
